de83dd210681d20c64597c607926986f8a626b77
Michael Panlilio <mtpanlilio20@hotmail.com>
2026-06-02 18:28:42 +0100

    Update - Main components built and routes configured

--- warehouse-dashboard/src/app/app.component.ts
+++ warehouse-dashboard/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NotificationBarComponent } from "./features/products/components/notification-bar/notification-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NotificationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
