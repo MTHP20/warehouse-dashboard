a0780224b488788acf7eb36da322edd9f0c7fb92
Michael Panlilio <mtpanlilio20@hotmail.com>
2026-06-02 20:43:04 +0100

    Update - Bootstrap installation

--- warehouse-dashboard/src/app/features/products/components/product-list/product-list.component.ts
+++ warehouse-dashboard/src/app/features/products/components/product-list/product-list.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductService } from '../../../../core/services/product.service';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BehaviorSubject, combineLatest, map, startWith } from 'rxjs';
+
+type sortOrder = 'asc' | 'desc';
 
 @Component({
   selector: 'app-product-list',
@@ -7,5 +13,27 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.scss',
 })
 export class ProductListComponent {
+  private router = inject(Router);
+  private productService = inject(ProductService);
+
+  categoryFilter = new FormControl('');
+  statusFilter = new FormControl('');
+
+  private sortOrder$ = new BehaviorSubject<sortOrder>('asc');
+  currentSort: sortOrder = 'asc';
+
+  private products = this.productService.getProducts();
+
+  // filteredProducts$ = combineLatest([
+  //   this.productService.getProducts(),
+  //   this.categoryFilter.valueChanges.pipe(startWith('')),
+  //   this.statusFilter.valueChanges.pipe(startWith('')),
+  //   this.sortOrder$
+  // ]).pipe(
+  //   map(([products, category, status, sort]) =>
+  //     this.applyFiltersAndSort(products, category ?? '', status ?? '', sort)
+  //   )
+  // );
+
 
 }
